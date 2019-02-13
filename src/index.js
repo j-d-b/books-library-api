921b213cee0d8e6b23c9438db5eb6c0c8c6ae849
j-d-b <jacob@jdbrady.info>
2019-02-12 22:43:11 -0500

    feat: delete and update/rename books (DELETE, PATCH)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const getBookList = require('./getBookList');
 
 const app = express();
-const bookList = [];
+let bookList = [];
 
 app.use(bodyParser.json());
 
@@ -12,12 +12,40 @@ app.post('/books', (req, res) => {
   const newBook = req.body.book;
 
   if (bookList.includes(newBook)) {
-    // TODO send 400 status
-    throw new Error('Duplicate book: That book already exists in the library');
+    res.status(400);
+    throw new Error('Duplicate Book: That book already exists in the library');
   }
 
-  bookList.push(newBook);
+  bookList = [...bookList, newBook];
   res.send({ book: newBook });
+});
+
+app.delete('/books', (req, res) => {
+  const bookToDelete = req.body.book;
+
+  if (!bookList.includes(bookToDelete)) {
+    res.status(400);
+    throw new Error('Not Found: Book does not exist in the library');
+  }
+
+  bookList = bookList.filter(book => book !== bookToDelete);
+  res.status(204).send();
+});
+
+app.patch('/books', (req, res) => {
+  const { originalBook, newBook } = req.body;
+
+  if (!bookList.includes(originalBook)) {
+    res.status(400);
+    throw new Error('Not Found: Book does not exist in the library');
+  }
+  if (bookList.includes(newBook)) {
+    res.status(400);
+    throw new Error('Duplicate Book: That book already exists in the library');
+  }
+
+  bookList = bookList.map(book => book === originalBook ? newBook : book);
+  res.status(204).send();
 });
 
 app.get('/books', (req, res) => {
